0d565f26197d4abe1a3256ad40cad34c3bc7fad7
Sawyer Travis <sawtravis@gmail.com>
2023-01-23 16:00:26 -0500

    not working: error about qrcodeId;

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 // https://www.youtube.com/watch?v=06pWsB_hoD4&ab_channel=BenAwad
 import React, { useEffect } from "react";
+import Html5QrcodePlugin from "./Html5QrcodePlugin";
+// function App() {
+//   useEffect(() => {
+//     // code to get data from backend database
+//     fetch("/api/packages", {
+//       method: 'GET',
+//       mode: 'cors'
+//     }).then(response =>
+//       response.json().then(data => {
+//         console.log(data.packages);
+//       })
+//     );
+//   }, []);
 
-function App() {
+//   return (
+//     <div className="App">Hello World!</div>
+//   );
+// }
+
+const App = () => {
+  let html5QrCode;
+  
   useEffect(() => {
-    // code to get data from backend database
-    fetch("/api/packages", {
-      method: 'GET',
-      mode: 'cors'
-    }).then(response =>
-      response.json().then(data => {
-        console.log(data.packages);
-      })
-    );
+      // Anything in here is fired on component mount.
+      if(!html5QrCode?.getState()){
+          html5QrCode = new Html5QrcodePlugin(qrcodeId);
+          const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+              /* handle success */
+          };
+          const config = { fps: 10, qrbox: { width: 250, height: 250 }, aspectRatio: 1.777778};
+
+          // If you want to prefer back camera
+          html5QrCode.start(
+              { facingMode: "environment" },
+              config,
+              qrCodeSuccessCallback
+          );
+      }
+
+      return () => {
+          // Anything in here is fired on component unmount.
+
+      };
   }, []);
 
-  return (
-    <div className="App">Hello World!</div>
-  );
-}
+  return <div id={qrcodeId}></div>;
+};
 
 export default App;
