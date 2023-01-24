8d2df5af1b05dd35377b6c7b31108199f301ba8a
Peter Peng <yser.pc@gmail.com>
2023-01-24 17:09:44 -0500

    recovered documentation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from "react";
 import Html5Qrcode from "./Html5Qrcode";
 
 const App = () => {
+  // useEffect(() => {
+  //   // code to get data from backend database
+  //   fetch("/api/packages", {
+  //     method: 'GET',
+  //     mode: 'cors'
+  //   }).then(response =>
+  //     response.json().then(data => {
+  //       console.log(data.packages);
+  //     })
+  //   );
+  // }, []);
+
   return (
     <div>
       <Html5Qrcode />
