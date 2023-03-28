0eb682773f881d28582fdfd2f3a1caaa0bb7efc1
Peter Peng <yser.pc@gmail.com>
2023-03-28 14:52:08 -0400

    added Calvin logo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Content from '../components/Content'
 import Header from '../components/Header'
+import logo from '../asset/CalvinUniv-Horiz-full-color-positive.png'
+import Box from '@mui/material/Box';
 
 const Home = () => {
   return (
     <>
       <Header text="Package Locker" root={true} />
       <Content />
+      <Box sx={{
+        position: 'fixed',
+        width: '100%',
+        pt: '2vh',
+        pb: '2vh',
+        bottom: 0,
+        backgroundColor: 'white'
+      }}>
+        <img
+          src={logo}
+          alt="Calvin University Logo"
+          style={{
+            display: 'block',
+            margin: '0 auto',
+            width: '50vw'
+          }} />
+      </Box>
     </>
   )
 }
