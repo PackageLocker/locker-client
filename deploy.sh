735baca8345115bc8e11a660f3fdb092488c3766
Sawyer Travis <sawtravis@gmail.com>
2023-03-29 15:59:13 -0400

    update IP

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Building app..."
 npm run build
 
 echo "Deploying..."
-scp -r build/* locker1@153.106.168.84:/var/www/locker-client/
+scp -r build/* locker1@153.106.168.50:/var/www/locker-client/
 # scp -r build/* pi@10.0.0.215:/var/www/locker-client/
 
 echo "Done!"
